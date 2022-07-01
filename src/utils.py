6b7081eab56952f2a32e8cb2a0dc6ef3cab40ce1
1murda <gonzalo[1;7rfi.uba.ar>
2022-07-01 19:13:31 -0300

    fix typing

--- src/utils.py
+++ src/utils.py
@@ -2,24 +2,27 @@ import os
 from tekore import Spotify
 
 # direccion del path principal
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR: str = os.path.dirname(os.path.dirname(__file__))
 
 
 # tekore .cfg path
-FILE_TEKORE = os.path.join(BASE_DIR, "tekore.cfg")
+FILE_TEKORE: str = os.path.join(BASE_DIR, "tekore.cfg")
 
 
 # carpeta donde se exportan las playlists
-PLAYLISTS_DIR = os.path.join(BASE_DIR, "playlists")
+PLAYLISTS_DIR: str = os.path.join(BASE_DIR, "playlists")
 
 # credenciales de la api spotify
-CLIENT_ID_SPOTIFY = '1e0c8ce0ab5c4bb6a4bdf6735ab9e950'
-CLIENT_SECRET_SPOTIFY = '9ecfcf4d27d94a4e908c644b45b68f84'
-REDIRECT_URI_SPOTIFY = 'https://localhost:8000/callback'
+CLIENT_ID_SPOTIFY: str = '1e0c8ce0ab5c4bb6a4bdf6735ab9e950'
+CLIENT_SECRET_SPOTIFY: str = '9ecfcf4d27d94a4e908c644b45b68f84'
+REDIRECT_URI_SPOTIFY: str = 'https://localhost:8000/callback'
+
+# credenciales de youtube
+API_KEY_YOUTUBE: str = "AIzaSyDxBafqudDX7YE7N5SJww_3OxIESSN77s8"
 
 # credenciales de la api de genius
-CLIENT_ID_GENIUS = "fYI4WYtFKPrbhG40VGPhGd2rv7pLBmMCpjIG3mgrl1JmXxcESW6YozwTg7CjvZu_"
-CLIENT_SECRET_GENIUS = "7oaL53G1sgCaATqX1bXcrtA7uj9taxe6KhCPXfnyV7Xof9BXSqCVWF5E_284OmXQVRAMV_WPyusMNyA5p0e6PA"
+CLIENT_ID_GENIUS: str = "fYI4WYtFKPrbhG40VGPhGd2rv7pLBmMCpjIG3mgrl1JmXxcESW6YozwTg7CjvZu_"
+CLIENT_SECRET_GENIUS: str = "7oaL53G1sgCaATqX1bXcrtA7uj9taxe6KhCPXfnyV7Xof9BXSqCVWF5E_284OmXQVRAMV_WPyusMNyA5p0e6PA"
 
 
 def cls() -> None:
